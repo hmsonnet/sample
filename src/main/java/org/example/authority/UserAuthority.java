238463444baa306d6e6a522b6bbffc0e3a710d41
손희문 <hmsonnet@gmail.com>
2024-01-16 17:51:11 +0900

    ..

---
+++ src/main/java/org/example/authority/UserAuthority.java
@@ -0,0 +1,29 @@
+package org.example.authority;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.example.user.service.User;
+import org.hibernate.annotations.Comment;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_user_authority")
+public class UserAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("아이디")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "authority_id")
+    private Authority authority;
+}
