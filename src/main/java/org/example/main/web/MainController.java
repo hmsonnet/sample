0e3445882857a0eb6825fd69edd34b894284c346
손희문 <hmsonnet@gmail.com>
2024-01-23 17:46:52 +0900

    ..

--- src/main/java/org/example/main/web/MainController.java
+++ src/main/java/org/example/main/web/MainController.java
@@ -8,8 +8,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,27 +17,26 @@ public class MainController {
 
     Logger logger = org.slf4j.LoggerFactory.getLogger(MainController.class);
 
-    @GetMapping(value = {"/", "/index"})
+    @GetMapping({"/index", "/"})
     public String index(
             HttpServletRequest request
             , HttpServletResponse response
             , Model model
     ) {
 
+        /*
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            // 사용자 정보 얻기
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserDetails) {
+                UserDetails userDetails = (UserDetails) principal;
+                logger.info("Username: {}", userDetails.getUsername());
+                logger.info("Authorities: {}", userDetails.getAuthorities());
+                // 여기서 필요한 다른 세션 정보를 얻을 수 있음
+            }
+        }*/
 
         return "/index";
-    }
-
-
-    @PostMapping("/validateLogin")
-    @ResponseBody
-    public String validateLogin(
-            HttpServletRequest request
-            , HttpServletResponse response
-    ) {
-
-
-
-        return null;
     }
 }
