ee737463b0233d183bce25bfedb86c0d5a60b35a
손희문 <hmsonnet@gmail.com>
2024-01-26 14:19:35 +0900

    JSON으로 스프링시큐리티 사용할 수 있도록 수정

--- src/main/java/org/example/filter/JsonAuthenticationFilter.java
+++ src/main/java/org/example/filter/JsonAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package org.example.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,9 +14,13 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public class JsonAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private static final String SECRET_KEY = "ASDFASFQWER3425243564236423563425GSDFGDFBGFSDFGSDFGDSFGDSFGWERTQQWEAZSXCVX";
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -53,6 +59,27 @@ public class JsonAuthenticationFilter extends UsernamePasswordAuthenticationFilt
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
                                             FilterChain chain, Authentication authResult) throws IOException, ServletException {
+
+        // 토큰 만료 시간 설정
+        Date expireTime = new Date(System.currentTimeMillis() + (1000 * 60 * 60 * 10)); // 10시간 후 만료
+
+        // 헤더 설정
+        Map<String, Object> headerClaims = new HashMap<>();
+        headerClaims.put("typ", "JWT");
+        headerClaims.put("alg", "HS256");
+
+        // 토큰 생성
+        String token = JWT.create()
+                .withHeader(headerClaims) // 헤더 설정
+                .withClaim("username", authResult.getName()) // payload에 저장될 정보
+                .withClaim("role", authResult.getAuthorities().toArray()[0].toString()) // payload에 저장될 정보
+                .withExpiresAt(expireTime) // 만료 시간 설정
+                .sign(Algorithm.HMAC256(SECRET_KEY)); // 시크릿 키를 이용한 서명
+
+
+        // 토큰을 response의 헤더에 저장
+        response.addHeader("Authorization", "Bearer " + token);
+
         super.successfulAuthentication(request, response, chain, authResult);
         chain.doFilter(request, response);
     }
